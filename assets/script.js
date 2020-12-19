3b3e8ace800aa734223de2030c4fee5b8ab16a09
Nick May <nickmay9@outlook.com>
2020-12-18 15:19:42 -1000

    edited main profile picture and about section

--- assets/script.js
+++ assets/script.js
@@ -1,37 +1,37 @@
 //object array to hold project info
 var projectObjArr = [
     {
-        source: "./assets/images/Image 12-4-20 at 2.49 PM.jpeg",
+        source: "./assets/images/projects/Image 12-4-20 at 2.49 PM.jpeg",
         title: "Movie App",
         code: "HTML/CSS/UIKit/JavaScript/Youtube-API/OMDB-API",
         link: "https://nickmay9.github.io/MovieApp-TeamProject1/"
     },
     {
-        source: "./assets/images/Image 12-4-20 at 3.01 PM.jpeg",
+        source: "./assets/images/projects/Image 12-4-20 at 3.01 PM.jpeg",
         title: "What's the Weather",
         code: "HTML/CSS/JQuery",
         link: "https://nickmay9.github.io/weatherApp-challenge6/"
     },
     {
-        source: "./assets/images/Image 10-23-20 at 10.24 AM.jpeg",
+        source: "./assets/images/projects/Image 10-23-20 at 10.24 AM.jpeg",
         title: "Run Buddy",
         code: "HTML/CSS",
         link: ""
     },
     {
-        source: "./assets/images/project-3.jpeg",
+        source: "./assets/images/projects/project-3.jpeg",
         title: "Project 4",
         code: "HTML/CSS",
         link: ""
     },
     {
-        source: "./assets/images/project-4.jpeg",
+        source: "./assets/images/projects/project-4.jpeg",
         title: "Project 5",
         code: "HTML/CSS",
         link: ""
     },
     {
-        source: "./assets/images/project-5.jpeg",
+        source: "./assets/images/projects/project-5.jpeg",
         title: "Project 6",
         code: "HTML/CSS",
         link: ""
